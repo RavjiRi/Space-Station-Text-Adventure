02a8d5bdde97869e71f3c011798c02ec7d73ceb8
ravjiri <ravjiri@whs.school.nz>
2023-05-04 11:39:42 +1200

    Set up github system

---
+++ Space_Station_Text_Adventure.java
@@ -0,0 +1,19 @@
+/**
+ * Text adventure where you have to divert the space station from collision course
+ * with a meteor
+ *
+ * @author Ritesh Ravji
+ * @version 4/5/23
+ */
+public class Space_Station_Text_Adventure
+{
+    // instance variables - replace the example below with your own
+
+    /**
+     * Constructor for objects of class Space_Station_Text_Adventure
+     */
+    public Space_Station_Text_Adventure()
+    {
+        // initialise instance variables
+    }
+}
