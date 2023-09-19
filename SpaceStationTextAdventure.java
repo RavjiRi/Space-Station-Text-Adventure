bc6a3def8f6bc509700333778157e1cb2b44b4ed
ravjiri <ravjiri@whs.school.nz>
2023-09-19 13:02:25 +1200

    Final commit Comment spacing improved

--- SpaceStationTextAdventure.java
+++ SpaceStationTextAdventure.java
@@ -1,6 +1,7 @@
 /**
  * Text adventure where you have to divert the space station from collision course
  * with a meteor.
+ * 
  * the text adventure can be changed by editing the project files
  * for example, to change the item descriptions, go into the item descriptions folder and edit the txt file
  *
